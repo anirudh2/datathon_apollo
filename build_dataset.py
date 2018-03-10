3bfbd822700b513e1aaf64561efbb3db82f7084d
anirudh2 <patel.anirudh0025@gmail.com>
2018-03-10 03:40:11 -0800

    Working version of build_dataset.py as per specifications discussed

--- build_dataset.py
+++ build_dataset.py
@@ -9,7 +9,16 @@ import pdb
 # Create parser and add an argument to specify which directory the data is in
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', default='data/2017CHR_CSV_Analytic_Data.csv',help="Dataset spreadsheet")
-parser.add_argument('--columns_to_read', default=[0],help="Which columns to read from the spreadsheet")
+parser.add_argument('--columns_to_read', default=[6, 21],help="Which columns to read from the spreadsheet")
+
+# Check if a string represents an int
+def repsInt(test_str):
+    try:
+        int(test_str)
+        return True
+    except ValueError:
+        return False
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
@@ -23,42 +32,36 @@ if __name__ == '__main__':
     elif filename.endswith('.csv'):
         df = pd.read_csv(filename)
 
+    # Convert from pandas to numpy array
     full_array = df.values
     array_size = full_array.shape
     A = []
-    for i in range(2,array_size[0]):
-        if int(full_array[i,2]) == 0:
-            for col_num in range(len(args.columns_to_read)):
-                col = args.columns_to_read[col_num]
-                temp_str = full_array[i,col]
-                temp = float(temp_str.replace(',',''))
-                if i == 2:
-                    A.append([temp])
-                else:
-                    A[col_num].append(temp)
+    # Use later to figure out if we want to
+    first_data_ix = 100000
+    # Loop over rows of the full array
+    for i in range(0,array_size[0]):
+        if (repsInt(full_array[i,2])):
 
-
-                # A.append(temp)
-                pdb.set_trace()
-
-#     A = []
-#
-#         all_values = df.values
-#         array_size = all_values.shape
-#         premature_deaths = []
-#         poor_mental_health_days = []
-#         # pdb.set_trace()
-#         # A = np.empty((0,51))
-#         for i in range(1,array_size[0]):
-#             if  int(all_values[i,2]) == 0:
-#                 print(all_values[i,3])
-#                 temp_str = all_values[i,6]
-#                 temp = int(temp_str.replace(',',''))
-#                 premature_deaths.append(temp)
-#                 poor_mental_health_days.append(float(all_values[i,21]))
-#
-#         A = [premature_deaths]
-#         A.append([poor_mental_health_days])
-#         A = np.asarray(A)
-#         pdb.set_trace()
-# #
+            # if the 3rd column (country code) is 0, we are looking at a number for a
+            # state. This is what we want. Is this the case in other datasets?
+            if int(full_array[i,2]) == 0:
+                for col_num in range(len(args.columns_to_read)):
+                    col = args.columns_to_read[col_num]
+                    # Check if the data is a string. If not, write directly to temp (assumes a float)
+                    if (type(full_array[i,col]) is str):
+                        temp_str = full_array[i,col]
+                        # The data has commas. Remove to cast to float
+                        try:
+                            temp = float(temp_str.replace(',',''))
+                        except AttributeError:
+                            print('Trying to use replace on temp_str when it is not a str')
+                            pdb.set_trace()
+                    else:
+                        temp = float(full_array[i,col])
+                    # Make a new row in our A matrix if we are in the first entry
+                    # Else add to existing rows
+                    if not A or (i == first_data_ix):
+                        A.append([temp])
+                        first_data_ix = i
+                    else:
+                        A[col_num].append(temp)
