2b8d61bb5686c037af8dc67065d25d993f9194bf
NiuNiuChen <lengleng@umich.com>
2018-03-10 09:01:17 -0800

    linear reg taking multiple features

--- linearRegression.py
+++ linearRegression.py
@@ -15,20 +15,28 @@ gen_func = util.generate_A_and_b()
 A, y = gen_func.generate() # Make A, b once we figure out what file we want to use for b
 #pdb.set_trace()
 
-A = np.asarray(A);
-A_oneP = A[0] 
-A_mat = array([A_oneP.T, ones(50)])
-y_tmp = y[0]
+A = np.asarray(A)
+#A_oneP = A[0] 
+#A_mat = array([A.T, ones(50)])
+#y_oneP = y[0]
+ones_vec = np.ones((1,50))
+A_mat = np.r_[A,ones_vec]
 w = linalg.lstsq(A_mat.T,y[0])[0] # Don't need to transpose A. Also, why are we taking the value at 0?
 
-
-xi = arange(0,9)
-C = array([ xi, ones(9)])
-# linearly generated sequence
-m = [19, 20, 20.5, 21.5, 22, 23, 23, 25.5, 24]
-w = linalg.lstsq(C.T,m)[0] # obtaining the parameters
-
 # plotting the line
-line = w[0]*xi+w[1] # regression line
-plot(xi,line,'r-',xi,m,'o')
+line = np.matmul(A_mat.T, w) # regression line
+plot(y[0],line,'o')#,A_mat.T,y[0],'o')
+print(w)
 show()
+
+
+#xi = arange(0,9)
+#C = array([ xi, ones(9)])
+## linearly generated sequence
+#m = [19, 20, 20.5, 21.5, 22, 23, 23, 25.5, 24]
+#w = linalg.lstsq(C.T,m)[0] # obtaining the parameters
+#
+## plotting the line
+#line = w[0]*xi+w[1] # regression line
+#plot(xi,line,'r-',xi,m,'o')
+#show()
