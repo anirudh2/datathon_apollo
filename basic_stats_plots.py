a4f7699fd23cb7b0d78e7c1c6de812830bb3c2bb
anirudh2 <patel.anirudh0025@gmail.com>
2018-03-10 16:29:02 -0800

    Added pylab.show()

--- basic_stats_plots.py
+++ basic_stats_plots.py
@@ -9,20 +9,19 @@ import pylab
 import util
 import numpy as np
 import matplotlib.pyplot as plt
+import pdb
 
 #gen_func = util.generate_A_and_b()
 gen_func = util.generate_A_census()
-A, y = gen_func.generate() 
+A, y = gen_func.generate()
 
 A = np.asarray(A)
 A_oneVect = np.sum(A,1)
-
+# pdb.set_trace()
 pylab.plot(np.arange(2000,2000+len(A_oneVect),1),A_oneVect,'o')
-
-plt.hist(A_oneVect)
-plt.title("Histogram")
-plt.xlabel("Housing")
-plt.ylabel("Frequency")
-
-
-
+pylab.show()
+#
+# plt.hist(A_oneVect)
+# plt.title("Histogram")
+# plt.xlabel("Housing")
+# plt.ylabel("Frequency")
